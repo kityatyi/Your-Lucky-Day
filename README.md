d0e885abab84985637351750a9aa989ee0fd6522
kityatyi <zsmonostori80@gmail.com>
2020-12-04 03:05:59 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Nothing special here. Just a training project, experimenting with Math.random and if statements in JavaScript. 
+
+Zsolt
+
+zsmonostori80@gmail.com
---
+++ js/35.js
@@ -0,0 +1,31 @@
+function luckyDay() {
+  n = "Wanna know your lucky day?";
+  document.getElementById("lucky-day").innerText = n;
+}
+
+function dayOfWeek() {
+  var n = Math.random();
+  // prettier-ignore
+  n = (n * 7) + 1;
+  n = Math.floor(n);
+
+  if (n === 1) {
+    n = "Monday";
+  } else if (n === 2) {
+    n = "Tuesday";
+  } else if (n === 3) {
+    n = "Wednesday";
+  } else if (n === 4) {
+    n = "Thursday";
+  } else if (n === 5) {
+    n = "Friday";
+  } else if (n === 6) {
+    n = "Saturday";
+  } else {
+    n = "Sunday";
+  }
+
+  document.getElementById("lucky-day").innerText = n;
+
+  setTimeout(luckyDay, 1666);
+}
